9cf4c30f43f14aba8b4dcbb13ffae6bf0203d712
OmarAdel24 <omar03adel2003@gmail.com>
2026-06-17 21:04:35 +0300

    Apply official go documentation workflow and add artifacts

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 package main
-import (
-    "fmt"
-    "net/http"
-)
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
-}
+import "fmt"
+
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Server starting on port 3000...")
-    http.ListenAndServe(":3000", nil)
+    fmt.Println("Hello from GitHub Actions!")
 }
